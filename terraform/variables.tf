899d25a6a819670909ead0d90a3cd1c49d4f08f1
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-24 21:27:41 +0530

    Add variables for location and resource group name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "centralindia"
+}
 
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+  default     = "rg-cosmos-query-lab"
+}
